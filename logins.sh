5308fada4636bdda477b9949096b7b8d5cd9e904
Alencea <67931836+Alencea@users.noreply.github.com>
2022-11-16 13:03:56 -0500

    Add files via upload

---
+++ logins.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#######################
+##
+## shelltemplate.sh
+##	Author:		Iain
+##	Date:		11/16/2022
+##	last Revised:	11/16/2022
+##	description:	
+##
+#######################
+
+#colored text
+DEFAULT="\e[39m"
+RED="\e[31m"
+BULE="\e[34m"
+GREEN="\e[32m"
+PURPLE="\e[35m"
+CYAN="\e[36m"
+horiz_line="============================"
+
+##     Error checking ###############
+if [[ $# -ne 1]]
+then
+  echo -e $RED"Error Message"$DEFAULT
+  exit 1
+fi
+#####################################
+
+
+
+exit 0
+#####################################
+
+
