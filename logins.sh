1ca9a66902b8f14dbcae3cfbe2a93471590e84d3
Alencea <67931836+Alencea@users.noreply.github.com>
2022-11-16 14:53:50 -0500

    Add files via upload

--- logins.sh
+++ logins.sh
@@ -2,7 +2,7 @@
 #######################
 ##
 ## shelltemplate.sh
-##	Author:		Iain
+##	Author:		Iain Gilley
 ##	Date:		11/16/2022
 ##	last Revised:	11/16/2022
 ##	description:	
@@ -19,14 +19,28 @@ CYAN="\e[36m"
 horiz_line="============================"
 
 ##     Error checking ###############
-if [[ $# -ne 1]]
+if [[ $# -ne 1 ]]
 then
   echo -e $RED"Error Message"$DEFAULT
   exit 1
 fi
 #####################################
 
+COUNT=0 # loop counter
+stuff=$(last -w | head -n -2 | cut -d' ' -f1 | sort | uniq )
+##echo $stuff | cut -d' ' -f1
+while read -r line
+do
+(( COUNT++ ))
+name=$stuff | cut -d' ' -f$COUNT
+if [ !-z "$name"]
+then
+echo $name
+fi
 
+
+done <$1
+echo "end"
 
 exit 0
 #####################################
